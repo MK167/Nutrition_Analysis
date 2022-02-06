a5e282ece7d1c98a53700b46cb882137f11ea49a
Mohammed Khaled Saad <58882363+MK167@users.noreply.github.com>
2022-02-07 01:34:55 +0200

    Add Unit Test

--- src/app/Components/nutrition/nutrition.component.spec.ts
+++ src/app/Components/nutrition/nutrition.component.spec.ts
@@ -1,25 +1,39 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { APP_BASE_HREF } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { NutritionComponent } from './nutrition.component';
 
 describe('NutritionComponent', () => {
   let component: NutritionComponent;
   let fixture: ComponentFixture<NutritionComponent>;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NutritionComponent ]
-    })
-    .compileComponents();
-  });
+      ],
+      declarations: [
+        NutritionComponent
+      ],
+      providers: [
+        {provide: APP_BASE_HREF, useValue: '/'},
+      ]
+    }).compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NutritionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(NutritionComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
